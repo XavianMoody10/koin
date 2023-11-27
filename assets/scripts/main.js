e5db5157ec805ce19f03410e19063d47a975c5cb
Xavian Moody <XavianMoody1025@gmail.com>
2023-11-27 18:29:41 -0500

    Made resuable javascript code for desktop and mobile header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,7 @@
 function intersectionObserver(options, element) {
   let elIsVisible;
 
-  // Intersection observer is asynchronous, so it needs to be placed inside a promise. This allows elIsVisble to be redeclared.
+  // Intersection observer is asynchronous, so it needs to be placed inside a promise. This allows elIsVisble to be redeclared before being returned.
   return new Promise((resolve) => {
     let callback = (entries, observer) => {
       entries.forEach((entry) => {
@@ -20,35 +20,47 @@ function intersectionObserver(options, element) {
 }
 
 // Change header to darker background when beginning scroll
-function desktopDarkHeader() {
+function darkHeaderAffectAnimation() {
   const desktopHeader = document.querySelector(".desktop-header");
+  const mobileHeader = document.querySelector(".mobile-header");
   const heroSection = document.querySelector(".hero-section");
-  const darkClass = "desktop-header--dark";
+  const desktopDarkClass = "desktop-header--dark";
+  const mobileDarkClass = "mobile-header--dark";
   const defaultClass = "desktop-header--default";
 
-  const options = {
-    threshold: 0.1,
-  };
+  // Execute animation on header based on conditions
+  function animationEffect(headerEl, darkClass) {
+    const elDisplay = window.getComputedStyle(headerEl, null).display;
+
+    const options = {
+      threshold: 0.1,
+    };
+
+    if (elDisplay !== "none") {
+      intersectionObserver(options, heroSection).then((isVisible) => {
+        if (!isVisible) {
+          if (
+            !headerEl.classList.contains(darkClass) &&
+            !headerEl.classList.contains(defaultClass)
+          ) {
+            headerEl.classList.add(darkClass);
+          } else {
+            headerEl.classList.remove(defaultClass);
+            headerEl.classList.add(darkClass);
+          }
+        } else {
+          if (headerEl.classList.contains(darkClass)) {
+            headerEl.classList.remove(darkClass);
+            headerEl.classList.add(defaultClass);
+          }
+        }
+      });
+    }
+  }
 
   window.addEventListener("scroll", () => {
-    intersectionObserver(options, heroSection).then((isVisible) => {
-      if (!isVisible) {
-        if (
-          !desktopHeader.classList.contains(darkClass) &&
-          !desktopHeader.classList.contains(defaultClass)
-        ) {
-          desktopHeader.classList.add(darkClass);
-        } else {
-          desktopHeader.classList.remove(defaultClass);
-          desktopHeader.classList.add(darkClass);
-        }
-      } else {
-        if (desktopHeader.classList.contains(darkClass)) {
-          desktopHeader.classList.remove(darkClass);
-          desktopHeader.classList.add(defaultClass);
-        }
-      }
-    });
+    animationEffect(desktopHeader, desktopDarkClass);
+    animationEffect(mobileHeader, mobileDarkClass);
   });
 }
 
@@ -60,17 +72,22 @@ function testimonialSliderAnimation() {
   const arrows = document.querySelectorAll(".slider-controls__arrow");
   const backwardArrow = arrows[0];
   const forwardArrow = arrows[1];
+
+  // Get the entire width of the slider
+  // Get the amont of slides in the slider
+  // We must take into account the spacing between the slides, which is 10px
+  // So, divide the slider's width by the number of slides + 10px and it will give you the exact number of pixels to move the slider each time the slider is interacted (exposing one slide at a time).
   const translateCalc = sliderInner.offsetWidth / slides.length + 10;
 
-  // execute slide when called
+  // Execute slide animation
   function slide(num) {
     sliderPosition = sliderPosition + num;
     sliderInner.style.transform = `translateX(${sliderPosition}px)`;
   }
 
-  // Move slide forward
+  // Forward slide eventlistener
   forwardArrow.addEventListener("click", () => {
-    const max = -translateCalc * (slides.length % 2);
+    const max = -translateCalc * (slides.length - 1);
 
     if (sliderPosition === max) {
       return;
@@ -79,7 +96,7 @@ function testimonialSliderAnimation() {
     }
   });
 
-  // Move slide backwards
+  // Backward slide eventlistener
   backwardArrow.addEventListener("click", () => {
     if (sliderPosition === 0) {
       return;
@@ -89,5 +106,5 @@ function testimonialSliderAnimation() {
   });
 }
 
-desktopDarkHeader();
+darkHeaderAffectAnimation();
 testimonialSliderAnimation();
